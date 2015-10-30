a6f5eadbb3572f10c20159a0cca6667756c6ee1c
jpic <jamespic@gmail.com>
2015-10-30 23:22:41 +0100

    Repository rename

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='compotista-import-parltrack',
+    name='import-parltrack-representatives',
     version='0.0.1',
-    description='Base app for importing parltrack votes into compotista',
+    description='Import parltrack into django-representatives',
     author='Arnaud Fabre',
     author_email='webmaster@memopol.org',
     url='http://github.com/political-memory/compotista-import_parltrack',
